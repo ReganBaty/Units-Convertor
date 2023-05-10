715d348733812418b95a73083be790d9fb6f766c
Regan <regan@reganbaty.com>
2023-05-10 16:45:07 +1000

    More regex

--- src/tests.ts
+++ src/tests.ts
@@ -9,7 +9,7 @@ type Test = {
 };
 
 const pounds = {
-  regex: /((?:\d*\.*)\d+){1}\s*(?:lbs|pounds)/i,
+  regex: /((?:\d*\.*)\d+){1}\s*(?:lb|pound)/i,
   UnitConvertor: (num: number) => {
     const kgs = (num / 2.2046).toFixed(3);
     return `${kgs} kgs`;
@@ -18,9 +18,36 @@ const pounds = {
 
 const feet = {
   regex: /((?:\d*\.*)\d+){1}\s*(?:ft|feet|foot)/i,
+
   UnitConvertor: (num: number) => {
     const meters = (num / 3.2808).toFixed(3);
     return `${meters} m`;
+  },
+};
+
+const cups = {
+  regex: /((?:\d*\.*)\d+){1}\s*(?:cup)/i,
+  UnitConvertor: (num: number) => {
+    const ml = (num * 250).toFixed(0);
+    return `${ml} ml`;
+  },
+};
+
+const teaspoons = {
+  regex: /((?:\d*\.*)\d+){1}\s*(?:tsp|teaspoon)/i,
+  UnitConvertor: (num: number) => {
+    // Good system
+    const ml = (num * 4.928922).toFixed(0);
+    return `${ml} ml`;
+  },
+};
+
+const tableSpoons = {
+  regex: /((?:\d*\.*)\d+){1}\s*(?:tbsp|tablespoon)/i,
+  UnitConvertor: (num: number) => {
+    // Good system
+    const ml = (num * 14.8).toFixed(0);
+    return `${ml} ml`;
   },
 };
 
@@ -41,4 +68,10 @@ const MakeTest = (test: Test) => {
   };
 };
 
-export const tests = [MakeTest(pounds), MakeTest(feet)];
+export const tests = [
+  MakeTest(pounds),
+  MakeTest(feet),
+  MakeTest(cups),
+  MakeTest(teaspoons),
+  MakeTest(tableSpoons),
+];
