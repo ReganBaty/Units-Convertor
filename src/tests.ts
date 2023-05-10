0803e751c22e921b563fa9d9814063279b773a28
Regan <regan@reganbaty.com>
2023-05-10 15:52:38 +1000

    Rework tests page

--- src/tests.ts
+++ src/tests.ts
@@ -3,38 +3,42 @@ export type TestResult = {
   title?: string;
 };
 
-const pounds = (n: Node): TestResult => {
-  const regex = /((?:\d*\.*)\d+){1}\s*(?:lbs|pounds)/i;
-  const match = n.textContent?.match(regex);
+type Test = {
+  regex: RegExp;
+  UnitConvertor: (num: number) => string;
+};
 
-  if (!match) return { match };
+const pounds = {
+  regex: /((?:\d*\.*)\d+){1}\s*(?:lbs|pounds)/i,
+  UnitConvertor: (num: number) => {
+    const kgs = (num / 2.2046).toFixed(3);
+    return `${kgs} kgs`;
+  },
+};
 
-  const num = parseFloat(match[1]);
-  const kgs = (num / 2.2046).toFixed(3);
+const feet = {
+  regex: /((?:\d*\.*)\d+){1}\s*(?:ft|feet|foot)/i,
+  UnitConvertor: (num: number) => {
+    const meters = (num / 3.2808).toFixed(3);
+    return `${meters} m`;
+  },
+};
 
-  const title = `${kgs} kgs`;
+const MakeTest = (test: Test) => {
+  return (n: Node): TestResult => {
+    const regex = test.regex;
+    const match = n.textContent?.match(regex);
 
-  return {
-    match: match,
-    title,
+    if (!match) return { match };
+    const num = parseFloat(match[1]);
+
+    const title = test.UnitConvertor(num);
+
+    return {
+      match: match,
+      title,
+    };
   };
 };
 
-const feet = (n: Node): TestResult => {
-  const regex = /((?:\d*\.*)\d+){1}\s*(?:ft|feet|foot)/i;
-  const match = n.textContent?.match(regex);
-
-  if (!match) return { match };
-
-  const num = parseFloat(match[1]);
-  const meters = (num / 3.2808).toFixed(3);
-
-  const title = `${meters} m`;
-
-  return {
-    match: match,
-    title,
-  };
-};
-
-export const tests = [pounds, feet];
+export const tests = [MakeTest(pounds), MakeTest(feet)];
